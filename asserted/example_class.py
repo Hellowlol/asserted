8513e9cbeb0e2cd7f8be35c6c7267e9db182172d
Hellowlol <hellowlol1@gmail.com>
2017-07-03 20:59:02 +0200

    add bool.

--- asserted/example_class.py
+++ asserted/example_class.py
@@ -10,6 +10,8 @@ class Ex(object):
         self.is_tuple = (3, 2, 1)
         self.is_dict = {'b': 'b', 'a': 'a'}
         self.is_int = 1
+        self.is_true = True
+        self.is_false = False
         self.is_float = 0.5
         self.is_generator_expression = (i for i in range(2))
         self.is_datetime = datetime.datetime(1970, 1, 1)
